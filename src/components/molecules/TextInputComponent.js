8182e2fed6945c1082e69b19fd96f6fbe1c05464
Zhafran Zhabyansyah <zhafranku@gmail.com>
2020-11-28 21:08:15 +0700

    first commit

--- src/components/molecules/TextInputComponent.js
+++ src/components/molecules/TextInputComponent.js
@@ -5,7 +5,7 @@ const TextInputComponent = () => {
     return (
         <View>
             <Text style={{ fontSize: 12, color: '#4F4F4F', marginTop: 52 }}>Nama pengeluaran</Text>
-            <TextInput placeholder="Nasi goreng" onChange={(value) => setDataPengeluaran({ nama: value.nativeEvent.text })} placeholderTextColor="#BDBDBD" style={{ height: 40, borderColor: '#46B5A7', borderBottomWidth: 2, marginTop: 10 }} />
+            <TextInput placeholder="Nasi goreng" placeholderTextColor="#BDBDBD" style={{ height: 40, borderColor: '#46B5A7', borderBottomWidth: 2, marginTop: 10 }} />
         </View>
     )
 }
